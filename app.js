1e453a52d4f01162e048da7a74435c4786d8981d
fatboy2003 <34652858+fatboy2003@users.noreply.github.com>
2017-12-22 00:26:56 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+//create a model
+
+var Fruit = Backbone.Model.extend({
+	validate: function(options) {
+		if(options.quantity && !_.isNumber(options.quantity)){
+			return('quantity must be a number');
+			}}
+		});
+	
+var apple = new Fruit({
+	name: 'apple',
+	color: 'red',
+	//quantity: 'a lot'
+});	
+
+apple.on('error', function(model, error) {console.log(error);});
+
+
+
+apple.set('quantity', 'a lot');
+//console.log(apple.get('quantity'));
+
+var banana = new Fruit();
+
+banana.set('type', 'banana');
+banana.set('color', 'yellow');
+banana.set('condition', 'raw');
+banana.set('place', 'Brazil');
+apple.set('condition', 'moldy')
+
+//console.log(banana.description());
+
